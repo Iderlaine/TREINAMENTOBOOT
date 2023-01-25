5e03ed39b1dd8b33972de7fc1a464957a8ceaa8d
liKa011 <likarolineers@gmail.com>
2023-01-25 13:57:15 -0300

    Updates

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Web.Models;
 
 namespace Tarefas.Web.Contollers;
 
@@ -7,5 +8,15 @@ public class TarefasController : Controller
     public IActionResult Create() 
     {
         return View();
+    }
+
+    public IActionResult Index () {
+        var listaDeTarefas = new List<TarefaViewModel>()
+        {
+            new TarefaViewModel() {Titulo= "Escovar os dentes", Descricao = "Usar o sensodine de manhã e não a noite"},
+            new TarefaViewModel() { Titulo= "Arrumar a cama "},
+            new TarefaViewModel() { Titulo="Por o lixo para fora", Descricao ="Somente às terças"}
+        };
+        return View(listaDeTarefas);
     }
 }
